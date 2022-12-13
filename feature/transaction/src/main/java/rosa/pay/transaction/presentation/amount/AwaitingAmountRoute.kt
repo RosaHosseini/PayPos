24924e8135b9fddf34c09e44884247d48044c7d0
Rosa Hosseini <3427234-hosseini.simurg@users.noreply.gitlab.com>
2022-12-13 18:03:31 +0330

    Use collectAsStateWithLifecycle

--- feature/transaction/src/main/java/rosa/pay/transaction/presentation/amount/AwaitingAmountRoute.kt
+++ feature/transaction/src/main/java/rosa/pay/transaction/presentation/amount/AwaitingAmountRoute.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -16,7 +17,6 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,6 +24,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import rosa.pay.transaction.R
 import rosa.pay.transaction.presentation.TransactionViewModel
 import rosa.pay.app.ui.components.ActionButton
@@ -36,14 +37,15 @@ import rosa.pay.app.ui.theme.PayColor
 import rosa.pay.app.ui.theme.Typography
 import rosa.pay.transaction.model.Money
 
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun AwaitingAmountRoute(
     viewModel: TransactionViewModel,
     keyPadViewModel: KeyPadViewModel = hiltViewModel()
 ) {
     DisableBackButton()
-    val offeredMoney = Money(keyPadViewModel.number.collectAsState().value)
-    val isKeypadEnabled by keyPadViewModel.isKeypadEnabled.collectAsState()
+    val offeredMoney = Money(keyPadViewModel.number.collectAsStateWithLifecycle().value)
+    val isKeypadEnabled by keyPadViewModel.isKeypadEnabled.collectAsStateWithLifecycle()
     AwaitingAmountScreen(
         onSubmitAmount = viewModel::onSubmitAmount,
         isTransactionAmountPermitted = viewModel::isTransactionAmountPermitted,
